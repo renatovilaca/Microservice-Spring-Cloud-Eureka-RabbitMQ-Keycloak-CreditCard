8376c986bc5ebc7908f06f226a761031cfbe7eed
Renato Vilaça <renatovilaca@gmail.com>
2023-09-10 13:25:17 -0300

    Added initial code for Cloud Gateway Microservice project

---
+++ cloudgateway/src/main/java/io/github/renatovilaca/cloudgateway/CloudgatewayApplication.java
@@ -0,0 +1,28 @@
+package io.github.renatovilaca.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class CloudgatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		//Routers for discovery server
+		return builder
+				.routes()
+					.route(r -> r.path("/clients/**").uri("lb://client-microservice")) //redirect to the load balacing
+				.build();
+	}
+
+}
---
+++ cloudgateway/src/test/java/io/github/renatovilaca/cloudgateway/CloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renatovilaca.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
